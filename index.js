2329cc03a3d0deb1213e13caa9b454e290a70405
FiajRahman <fiaj15-3077@diu.edu.bd>
2024-08-17 15:02:48 +0600

    middleware

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 5000;
 
 // middleware
 
-app.use(cors())
-app.use(express.json())
+// app.use(cors())
+// app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.neywkpg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
